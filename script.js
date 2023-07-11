8bbf1f23cceef9698d84ed3a590eb6b1568dc9c0
jolynnk <jolynnkhoosx@gmail.com>
2023-07-11 11:27:38 +0800

    round 1, 2, 3 done

--- script.js
+++ script.js
@@ -5,52 +5,58 @@ const elementArray = document.querySelectorAll(".element");
 
 let round = 1; //refers to words game by default
 
+round2Button = document.getElementById("round2-button");
+round2Button.style.display = "none";
+
+round3Button = document.getElementById("round3-button");
+round3Button.style.display = "none";
+
 //ROUND 1
 const listOfWords = [
   "jumbled",
-  "simplistic",
-  "spill",
-  "concerned",
-  "reason",
-  "halting",
-  "harm",
-  "silky",
-  "beautiful",
-  "abaft",
-  "structure",
-  "physical",
-  "courageous",
-  "petite",
-  "friendly",
-  "spade",
-  "millennial",
-  "heart",
-  "constant",
-  "function",
-  "loving",
-  "expansion",
-  "abundant",
-  "encouraging",
-  "flowers",
-  "command",
-  "appliance",
-  "middle",
-  "available",
-  "sudden",
-  "slip",
-  "arrogant",
-  "aquatic",
-  "drum",
-  "shocking",
-  "boot",
-  "tease",
-  "zip",
-  "number",
-  "ambitious",
-  "attract",
-  "thought",
-  "spotty",
-  "reflect",
+  // "simplistic",
+  // "spill",
+  // "concerned",
+  // "reason",
+  // "halting",
+  // "harm",
+  // "silky",
+  // "beautiful",
+  // "abaft",
+  // "structure",
+  // "physical",
+  // "courageous",
+  // "petite",
+  // "friendly",
+  // "spade",
+  // "millennial",
+  // "heart",
+  // "constant",
+  // "function",
+  // "loving",
+  // "expansion",
+  // "abundant",
+  // "encouraging",
+  // "flowers",
+  // "command",
+  // "appliance",
+  // "middle",
+  // "available",
+  // "sudden",
+  // "slip",
+  // "arrogant",
+  // "aquatic",
+  // "drum",
+  // "shocking",
+  // "boot",
+  // "tease",
+  // "zip",
+  // "number",
+  // "ambitious",
+  // "attract",
+  // "thought",
+  // "spotty",
+  // "reflect",
 ];
 
 //pick out a random word from the list of words
@@ -68,18 +74,18 @@ function round1() {
 //ROUND 2
 const listOfEquations = [
   "5 * 7",
-  "40 / 8",
-  "37 * 2",
-  "1 + 2",
-  "3 + 8",
-  "4 + 5",
-  "2 * 6",
-  "14 * 3",
-  "20 / 2",
-  "7 * 7",
-  "8 * 8",
-  "10 * 10",
-  "68 - 30",
+  // "40 / 8",
+  // "37 * 2",
+  // "1 + 2",
+  // "3 + 8",
+  // "4 + 5",
+  // "2 * 6",
+  // "14 * 3",
+  // "20 / 2",
+  // "7 * 7",
+  // "8 * 8",
+  // "10 * 10",
+  // "68 - 30",
 ];
 
 //pick out random equation from list of equations
@@ -91,6 +97,31 @@ function getRandomEquation() {
 function round2() {
   for (let i = 0; i < elementArray.length; i++) {
     elementArray[i].innerText = getRandomEquation();
+    // elementArray[i].style.color = "blue";
+  }
+}
+
+//ROUND 3
+const colors = [
+  "blue",
+  "orange",
+  "pink",
+  "red",
+  "yellow",
+  "brown",
+  "black",
+  "green",
+  "grey",
+];
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function round3() {
+  for (let i = 0; i < elementArray.length; i++) {
+    elementArray[i].innerText = "Color";
+    elementArray[i].style.color = getRandomColor();
   }
 }
 
@@ -121,13 +152,41 @@ function checkNumber() {
   }
 }
 
+//check color accuracy
+function checkColor() {
+  for (const displayedColor of elementArray) {
+    if (playerInput.value == displayedColor.style.color) {
+      displayedColor.innerText = "";
+      currentScore++;
+      score.innerText = currentScore;
+    }
+  }
+}
+
+//check if all elements are typed
+function checkAllTyped() {
+  for (const checkElements of elementArray) {
+    console.log(checkElements.innerText);
+    if (round === 1 && checkElements.innerText == "") {
+      round2Button.style.display = "block";
+    } else if (round === 2 && checkElements.innerText == "") {
+      round3Button.style.display = "block";
+      round2Button.style.display = "none";
+    }
+  }
+}
+
 //to accept player input when enter button pressed. source: https://blog.devgenius.io/how-to-detect-the-pressing-of-the-enter-key-in-a-text-input-field-with-javascript-380fb2be2b9e
 playerInput.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     if (round === 1) {
       checkWord();
+      checkAllTyped();
     } else if (round === 2) {
       checkNumber();
+      checkAllTyped();
+    } else if (round === 3) {
+      checkColor();
     }
     playerInput.value = ""; //clears input field after enter button pressed
   }
@@ -138,18 +197,21 @@ let timerNumber = document.getElementById("timer-number");
 const timer = document.querySelector("timer");
 
 //create 30s countdown timer
-let timeLeft = 5;
+let timeLeft = 10;
 const timerText = document.getElementById("timer-fulltext");
 
 function countdown() {
   const timerId = setTimeout(countdown, 1000);
   timerNumber.innerText = timeLeft;
-  if (timeLeft > 0 && timeLeft < 31) {
+  if (timeLeft > 0 && timeLeft < 61) {
     timeLeft--;
   } else if (timeLeft === 0) {
-    // playerInput.hidden = true;
     timerText.innerText = "TIME'S UP!";
     timerText.style.color = "red";
+    playerInput.style.display = "none";
+    startButton.style.display = "none";
+    round2Button.style.display = "none";
+    round3Button.style.display = "none";
   }
 }
 
@@ -159,15 +221,21 @@ startButton.addEventListener("click", function () {
   countdown();
 });
 
-round2Button = document.getElementById("round2-button");
-
 round2Button.addEventListener("click", function () {
   round = 2;
   round2();
   countdown();
 });
 
-//reset timer to 30 when round 2 started
-//insert unique word into boxes
+round3Button.addEventListener("click", function () {
+  round = 3;
+  round3();
+  countdown();
+});
+
+//congrats word when finished game
+//do random equations and source for answer when answer is typed, not to calculate everything in advance
+//aesthetics - background, font type etc
+//insert unique word into boxes. either this or destroy only 1 of the items when typed
 //game over if not all words typed
 //source words from word generator. https://www.section.io/engineering-education/how-to-build-a-speedtyping-game-using-javascript/
